cf19aefa5d187804151422b65ef200e69751b12d
degreane <degreane@gmail.com>
2019-07-29 19:06:22 +0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+    name="GiaCom Acc",
+    version="0.1",
+    url="https://github.com/Degreane/adysys-acc",
+    author="Faysal Al-Banna",
+    author_email="degreane@gmail.com",
+    description="GiaCom AdySys Accounting System (ISP Billing Interface)",
+    requires=[
+        'sanic',
+        'sanic_session',
+        'lxml',
+        'pyquery',
+        'pymitter',
+        'gunicorn',
+        'sip',
+        'sysrsync',
+        'sqlalchemy',
+        'mongoengine',
+    ],
+    provides=['adysys_acc'],
+    zip_safe=False,
+)
